6eb6434147c27b2fd31a64d5d0651451d5bc6835
Sanita Č <70488653+sanitadev@users.noreply.github.com>
2024-06-07 01:14:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let menu=document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+    menu.classList.toggle('move');
+}
+
+const constraints = {
+    name: {
+        presence: { allowEmpty: false }
+    },
+    email: {
+        presence: { allowEmpty: false },
+        email: true
+    },
+    message: {
+        presence: { allowEmpty: false }
+    }
+};
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function (event) {
+  const formValues = {
+      name: form.elements.name.value,
+      email: form.elements.email.value,
+      message: form.elements.message.value
+  };
+
+  const errors = validate(formValues, constraints);
+
+  if (errors) {
+    event.preventDefault();
+    const errorMessage = Object
+        .values(errors)
+        .map(function (fieldValues) { return fieldValues.join(', ')})
+        .join("\n");
+
+    alert(errorMessage);
+  }
+}, false);
