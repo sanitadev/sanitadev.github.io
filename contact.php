0978c8959b6cacf539c95f036391b03efd8fa96b
Sanita Č. <70488653+sanitadev@users.noreply.github.com>
2024-08-16 14:43:09 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
+<?php
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -15,10 +17,11 @@ echo
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact form</title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
