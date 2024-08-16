1ca9d5390ed8926b4dc767b0eaa97eb319c2a632
Sanita Č. <70488653+sanitadev@users.noreply.github.com>
2024-08-16 14:41:15 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,7 +15,6 @@ echo
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact form</title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
